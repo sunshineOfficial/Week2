3b89d5d660a86ba4124c61d7430b39c47f655f71
sunshine <thesunshineofficial@gmail.com>
2022-10-03 20:26:14 +0300

    Task 4 completed

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿Messenger.PrintMessage("Hell to world!");
+Messenger.PrintMessage("300 bucks", "Van");
+
+static class Messenger
+{
+    public static void PrintMessage(string message)
+    {
+        Console.WriteLine($"New message: {message}");
+    }
+
+    public static void PrintMessage(string message, string sender)
+    {
+        Console.WriteLine($"New message: {message}\nFrom: {sender}");
+    }
+}
+
+class Rectangle
+{
+    public int a;
+    public int b;
+
+    public Rectangle() : this(1) { }
+    public Rectangle(int a) : this(a, 1) { }
+    public Rectangle(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+}
